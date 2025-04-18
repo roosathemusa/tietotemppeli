5bb29ff71975b38a5da45d767c0f062ce63e3e80
emiliakaarre <ekaarre24@students.oamk.fi>
2025-04-18 11:59:04 +0300

    created functions for checking the right answer and showing info if correct

--- ymppäpeli/code/ymppä.js
+++ ymppäpeli/code/ymppä.js
@@ -8,65 +8,100 @@ startBtn.addEventListener("click", function (e){
     gameView.classList.remove("d-none");
 });
 
+//Lista eläinten tiedoista
 const animals = [
     {
         name: "Isopanda",
         image: "./images/panda.png",
         correctEnvironment: "bambuforest",
-        fact: "Mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla. (Lähde: Animalia.bio)"
+        fact: "mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla. (Lähde: Animalia.bio)"
     },
     {
         name: "Leijona",
         image: "./images/leijona.jpg",
         correctEnvironment: "savanna",
-        fact: "Laumassa naarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin. (Lähde: Animalia.bio)"
+        fact: "laumassa leijonanaarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin. (Lähde: Animalia.bio)"
     },
     {
         name: "Tiikeri",
         image: "./images/tiikeri.jpg",
         correctEnvironment: "sademetsä",
-        fact: "Tiesitkö, että tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista. (Lähde: Animalia.bio)"
+        fact: "tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista. (Lähde: Animalia.bio)"
     },
     {
         name: "Saharansarvikyy",
         image: "./images/sarvikyy.jpg",
         correctEnvironment: "desert",
-        fact: "Saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa. (Lähde: Animalia.bio)"
+        fact: "saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa. (Lähde: Animalia.bio)"
     },
     {
         name: "Lamantiini",
         image: "./images/lamantiini.jpg",
         correctEnvironment: "marine",
-        fact: "Lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista. (Lähde: Animalia.bio)"
+        fact: "lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista. (Lähde: Animalia.bio)"
     },
     {
         name: "Keisaripingviini",
         image: "./images/pingviini.jpg",
         correctEnvironment: "antarctic",
-        fact: "Lisääntyessään naaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa. (Lähde: Animalia.bio)"
+        fact: "lisääntyessään keisaripingviininaaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa. (Lähde: Animalia.bio)"
     },
     {
         name: "Tulisalamanteri",
         image: "./images/salamanteri.jpg",
         correctEnvironment: "mediterrane",
-        fact: "Sen elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet. (Lähde: Animalia.bio)"
+        fact: "tulisalamanterin elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet. (Lähde: Animalia.bio)"
     },
     {
         name: "Muuttohaukka",
         image: "./images/jalohaukka.jpg",
         correctEnvironment: "tundra",
-        fact: "Muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h. (Lähde: Animalia.bio)"
+        fact: "muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h. (Lähde: Animalia.bio)"
     },
     {
         name: "Liito-orava",
         image: "./images/liito-orava",
         correctEnvironment: "taiga",
-        fact: "Paritteluaikaan uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa. (Lähde: Animalia.bio)"
+        fact: "paritteluaikaan liito-orava uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa. (Lähde: Animalia.bio)"
     },
     {
         name: "Aroseepra",
         image: "./images/seepra.jpg",
         correctEnvironment: "grassland",
-        fact: "Seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin. (Lähde: Wikipedia)"
+        fact: "seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin. (Lähde: Wikipedia)"
     }
 ]
+
+//Eläimen vaihto aina uudelle kysymykselle
+let currentIndex = 0;
+let currentAnimal = animals[currentIndex]
+
+function showAnimal() {
+    currentAnimal = animals[currentIndex];
+    document.getElementById("animal-name").textContent = currentAnimal.name;
+    document.getElementById("animal-pic").src = currentAnimal.image;
+}
+
+//Vastausvaihtoehdot
+document.querySelectorAll('.environment-option').forEach(option => {
+    option.addEventListener('click', function () {
+        const selectedEnvironment = this.dataset.environment;
+        checkAnswer(selectedEnvironment);
+    });
+});
+
+//Vastauksen tarkistus ja faktaboxi
+const factBox = document.getElementById("fact-box");
+const factText = document.getElementById("fact-txt");
+const nextButton = document.getElementById("next-button");
+
+function checkAnswer(selected) {
+    if (selected === currentAnimal.correctEnvironment) {
+        //kuva vihreä ja lisää pisteitä
+        factBox.classList.remove("d-none");
+        factText.textContent = currentAnimal.fact;
+    } else {
+        //kuva punainen, näytä oikea vastaus
+    }
+}
+
