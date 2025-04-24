afc2478451a2648f3f6cdc917aa0ce078453ea79
emiliakaarre <ekaarre24@students.oamk.fi>
2025-04-24 13:47:21 +0300

    question options chaged for each animal

--- ymppäpeli/code/ymppä.js
+++ ymppäpeli/code/ymppä.js
@@ -10,16 +10,16 @@ startBtn.addEventListener("click", function (e){
 
 //Lista eläinten tiedoista
 const animals = [
-    {name: "Isopanda", image: "./images/panda.png", correctEnvironment: "bambuforest", fact: "mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla."},
-    {name: "Leijona", image: "./images/leijona.jpg", correctEnvironment: "savanna", fact: "laumassa leijonanaarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin."},
-    {name: "Tiikeri", image: "./images/tiikeri.jpg", correctEnvironment: "rainforest", fact: "tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista."},
-    {name: "Saharansarvikyy", image: "./images/sarvikyy.jpg", correctEnvironment: "desert", fact: "saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa."},
-    {name: "Lamantiini", image: "./images/lamantiini.jpg", correctEnvironment: "marine", fact: "lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista."},
-    {name: "Keisaripingviini", image: "./images/pingviini.jpg", correctEnvironment: "antarctic", fact: "lisääntyessään keisaripingviininaaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa."},
-    {name: "Tulisalamanteri", image: "./images/salamanteri.jpg", correctEnvironment: "mediterrane", fact: "tulisalamanterin elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet."},
-    {name: "Muuttohaukka", image: "./images/jalohaukka.jpg", correctEnvironment: "tundra", fact: "muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h."},
-    {name: "Liito-orava", image: "./images/liito-orava.jpg", correctEnvironment: "taiga", fact: "paritteluaikaan liito-orava uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa."},
-    {name: "Aroseepra", image: "./images/seepra.jpg", correctEnvironment: "grassland", fact: "seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin."}
+    {name: "Isopanda", image: "./images/panda.png", correctEnvironment: "bambuforest", fact: "mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
+    {name: "Leijona", image: "./images/leijona.jpg", correctEnvironment: "savanna", fact: "laumassa leijonanaarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/aavikko.jpg", environment: "desert" }]},
+    {name: "Tiikeri", image: "./images/tiikeri.jpg", correctEnvironment: "rainforest", fact: "tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista.", options: [{ image: "./images/välimeri.jpg", environment: "mediterrane" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
+    {name: "Saharansarvikyy", image: "./images/sarvikyy.jpg", correctEnvironment: "desert", fact: "saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa.", options: [{ image: "./images/sademetsä.png", environment: "rainforest" }, { image: "./images/aavikko.jpg", environment: "desert" }, { image: "./images/tundra.jpg", environment: "tundra" }]},
+    {name: "Lamantiini", image: "./images/lamantiini.jpg", correctEnvironment: "marine", fact: "lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista.", options: [{ image: "./images/bambumetsä.png", environment: "rainforest" }, { image: "./images/marine.jpg", environment: "marine" }, { image: "./images/välimeri.jpg", environment: "mediterrane" }]},
+    {name: "Keisaripingviini", image: "./images/pingviini.jpg", correctEnvironment: "antarctic", fact: "lisääntyessään keisaripingviininaaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa.", options: [{ image: "./images/marine.jpg", environment: "marine" }, { image: "./images/välimeri.jpg", environment: "mediterrane" }, { image: "./images/antarctic.jpg", environment: "antarctic" }]},
+    {name: "Tulisalamanteri", image: "./images/salamanteri.jpg", correctEnvironment: "mediterrane", fact: "tulisalamanterin elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet.", options: [{ image: "./images/välimeri.jpg", environment: "mediterrane" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
+    {name: "Muuttohaukka", image: "./images/jalohaukka.jpg", correctEnvironment: "tundra", fact: "muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h.", options: [{ image: "./images/antarctic.jpg", environment: "antarctic" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/tundra.jpg", environment: "tundra" }]},
+    {name: "Liito-orava", image: "./images/liito-orava.jpg", correctEnvironment: "taiga", fact: "paritteluaikaan liito-orava uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/taiga.jpg", environment: "taiga" }, { image: "./images/tundra.jpg", environment: "tundra" }]},
+    {name: "Aroseepra", image: "./images/seepra.jpg", correctEnvironment: "grassland", fact: "seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin.", options: [{ image: "./images/ruohomaa.jpg", environment: "grassland" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/tundra.jpg", environment: "tundra" }]}
 ]
 
 //Muuttujat
@@ -30,7 +30,22 @@ function showAnimal() {
     currentAnimal = animals[currentIndex];
     document.getElementById("animal-name").textContent = currentAnimal.name;
     document.getElementById("animal-pic").src = currentAnimal.image;
+
+    const options = document.querySelectorAll('.environment-option');
+
+    currentAnimal.options.forEach((opt, index) => {
+        options[index].src = opt.image;
+        options[index].dataset.environment = opt.environment;
+    });
 }
+
+//Vastausvaihtoehdot
+document.querySelectorAll('.environment-option').forEach(option => {
+    option.addEventListener('click', function () {
+        const selectedEnvironment = this.dataset.environment;
+        checkAnswer(selectedEnvironment);
+    });
+});
 
 //Vastauksen tarkistus
 function checkAnswer(selected) {
@@ -86,14 +101,28 @@ document.getElementById("next-button").addEventListener("click", function (event
     }
 });
 
-//Vastausvaihtoehdot (tämä ratkaisu saatu ChatGPT:ltä)
-document.querySelectorAll('.environment-option').forEach(option => {
-    option.addEventListener('click', function () {
-        const selectedEnvironment = this.dataset.environment;
-        checkAnswer(selectedEnvironment);
-    });
-});
-
 function endGame() {
     alert("Peli päättyi! Kiitos pelaamisesta!");
+}
+
+
+
+let score = 0
+
+//oikein score++;
+
+//pisteet adan versio
+function showScore() {
+    resetState();
+    questionElement.innerHTML = `Sait ${score} pistettä`;
+
+    sessionStorage.setItem('ymppä', score);
+
+    nextButton.innerHTML = "Aloita alusta";
+    nextButton.style.display = "block";
+    
+    nextButton.onclick = () => {
+        startQuiz();
+        nextButton.onclick = null;
+    };
 }
