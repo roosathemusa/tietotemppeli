199e9a72a0b89dc6b3681a389625918e09a235f8
emiliakaarre <ekaarre24@students.oamk.fi>
2025-04-28 09:08:27 +0300

    animal enviroments changes in finnish

--- ymppäpeli/code/ymppä.js
+++ ymppäpeli/code/ymppä.js
@@ -10,16 +10,16 @@ const scoreTxt = document.getElementById("score");
 
 //Lista eläinten tiedoista
 const animals = [
-    {name: "Isopanda", image: "./images/panda.png", correctEnvironment: "bambuforest", fact: "mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
-    {name: "Leijona", image: "./images/leijona.jpg", correctEnvironment: "savanna", fact: "laumassa leijonanaarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/aavikko.jpg", environment: "desert" }]},
-    {name: "Tiikeri", image: "./images/tiikeri.jpg", correctEnvironment: "rainforest", fact: "tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista.", options: [{ image: "./images/välimeri.png",environment: "mediterrane" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
-    {name: "Saharansarvikyy", image: "./images/sarvikyy.jpg", correctEnvironment: "desert", fact: "saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa.", options: [{ image: "./images/sademetsä.png", environment: "rainforest" }, { image: "./images/aavikko.jpg", environment: "desert" }, { image: "./images/tundra.png", environment: "tundra" }]},
-    {name: "Lamantiini", image: "./images/lamantiini.jpg", correctEnvironment: "marine", fact: "lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista.", options: [{ image: "./images/bambumetsä.png", environment: "rainforest" }, { image: "./images/marine.png", environment: "marine" }, { image: "./images/välimeri.png", environment: "mediterrane" }]},
-    {name: "Keisaripingviini", image: "./images/pingviini.png", correctEnvironment: "antarctic", fact: "lisääntyessään keisaripingviininaaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa.", options: [{ image: "./images/marine.png", environment: "marine" }, { image: "./images/välimeri.png", environment: "mediterrane"}, { image: "./images/antarctic.jpg", environment: "antarctic" }]},
-    {name: "Tulisalamanteri", image: "./images/salamanteri.jpg", correctEnvironment: "mediterrane", fact: "tulisalamanterin elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet.", options: [{ image: "./images/välimeri.png", environment: "mediterrane" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/sademetsä.png", environment: "rainforest" }]},
-    {name: "Muuttohaukka", image: "./images/jalohaukka.jpg", correctEnvironment: "tundra", fact: "muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h.", options: [{ image: "./images/antarctic.jpg", environment: "antarctic" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/tundra.png", environment: "tundra" }]},
-    {name: "Liito-orava", image: "./images/liito-orava.jpg", correctEnvironment: "taiga", fact: "paritteluaikaan liito-orava uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa.", options: [{ image: "./images/bambumetsä.png", environment: "bambuforest" }, { image: "./images/taiga.jpg", environment: "taiga" }, { image: "./images/tundra.png", environment: "tundra" }]},
-    {name: "Aroseepra", image: "./images/seepra.png", correctEnvironment: "grassland", fact: "seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin.", options: [{ image: "./images/ruohomaa.jpg", environment: "grassland" }, { image: "./images/savanni.png", environment: "savanna" }, { image: "./images/tundra.png", environment: "tundra" }]}
+    {name: "Isopanda", image: "./images/panda.png", correctEnvironment: "bambumetsä", fact: "mustan turkkinsa alla pandan iho on musta ja vaaleanpunainen valkoisen turkin kohdalla.", options: [{ image: "./images/bambumetsä.png", environment: "bambumetsä" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/sademetsä.png", environment: "sademetsä" }]},
+    {name: "Leijona", image: "./images/leijona.jpg", correctEnvironment: "savanni", fact: "laumassa leijonanaarat ovat vastuussa metsästyksestä, kun taas urokset puolustavat reviiriä. Huolimatta tästä vastuun jakamisesta urokset syövät aina ensin.", options: [{ image: "./images/bambumetsä.png", environment: "bambumetsä" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/aavikko.jpg", environment: "aavikko" }]},
+    {name: "Tiikeri", image: "./images/tiikeri.jpg", correctEnvironment: "sademetsä", fact: "tiikerin turkin kuviointi näkyy vielä silloinkin, kun se on ajeltu. Tämä ei johdu ihon pigmentistä, vaan ihoon juurtuneista karvoista ja karvatupista.", options: [{ image: "./images/välimeri.png",environment: "välimeri" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/sademetsä.png", environment: "sademetsä" }]},
+    {name: "Saharansarvikyy", image: "./images/sarvikyy.jpg", correctEnvironment: "aavikko", fact: "saharansarvikyy varoittaa uhkaajaa sahamaisella äänellä, jonka se saa aikaan hankaamalla karheita kyljen harjasuomuja toisiinsa.", options: [{ image: "./images/sademetsä.png", environment: "sademetsä" }, { image: "./images/aavikko.jpg", environment: "aavikko" }, { image: "./images/tundra.png", environment: "tundra" }]},
+    {name: "Lamantiini", image: "./images/lamantiini.jpg", correctEnvironment: "meri", fact: "lamantiini eli kynsimanaatti eli manaatti (Trichechus manatus) on yksi kolmesta manaattien nisäkäsheimon lajista.", options: [{ image: "./images/bambumetsä.png", environment: "sademetsä" }, { image: "./images/marine.png", environment: "meri" }, { image: "./images/välimeri.png", environment: "välimeri" }]},
+    {name: "Keisaripingviini", image: "./images/pingviini.png", correctEnvironment: "antarctica", fact: "lisääntyessään keisaripingviininaaras munii yhden munan, jonka se vierittää koiraalle haudottavaksi. Sitten naaras lähtee merelle kalaisille vesille syömään ja viettää siellä talven. Koiras hautoo munaa jalkojensa päällä ihopoimun suojassa yhdeksän viikkoa.", options: [{ image: "./images/marine.png", environment: "meri" }, { image: "./images/välimeri.png", environment: "välimeri"}, { image: "./images/antarctic.jpg", environment: "antarctica" }]},
+    {name: "Tulisalamanteri", image: "./images/salamanteri.jpg", correctEnvironment: "välimeri", fact: "tulisalamanterin elinympäristöä ovat vesistöjen läheiset kosteat ja varjoisat lehtimetsäalueet.", options: [{ image: "./images/välimeri.png", environment: "välimeri" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/sademetsä.png", environment: "sademetsä" }]},
+    {name: "Muuttohaukka", image: "./images/jalohaukka.jpg", correctEnvironment: "tundra", fact: "muuttohaukka eli jalohaukka on maailman nopein eläin ja voi pystysuorassa syöksyssä kiitää jopa lähes 400 km/h.", options: [{ image: "./images/antarctic.jpg", environment: "antarctica" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/tundra.png", environment: "tundra" }]},
+    {name: "Liito-orava", image: "./images/liito-orava.jpg", correctEnvironment: "taiga", fact: "paritteluaikaan liito-orava uroksia voi olla yhden naaraan seurassa useampia, jolloin ne kilpailevat naaraasta ajamalla toisiaan takaa.", options: [{ image: "./images/bambumetsä.png", environment: "bambumetsä" }, { image: "./images/taiga.jpg", environment: "taiga" }, { image: "./images/tundra.png", environment: "tundra" }]},
+    {name: "Aroseepra", image: "./images/seepra.png", correctEnvironment: "ruohomaa", fact: "seeprat ovat aikaisessa sikiövaiheessa väriltään mustia ja valkoiset raidat kehittyvät vasta myöhemmin.", options: [{ image: "./images/ruohomaa.jpg", environment: "ruohomaa" }, { image: "./images/savanni.png", environment: "savanni" }, { image: "./images/tundra.png", environment: "tundra" }]}
 ]
 
 //Muuttujat
@@ -60,12 +60,15 @@ document.querySelectorAll('.environment-option').forEach(option => {
     });
 });
 
-//Vastauksen tarkistus
+//Vastauksen tarkistus ja palaute
 function checkAnswer(selected) {
     const resultBox = document.getElementById("result-box");
     const resultTxt = document.getElementById("result-txt");
+    const correctTxt = document.getElementById("correct-answer");
 
     resultBox.classList.remove("d-none");
+    correctTxt.textContent = '';
+    
     if (selected === currentAnimal.correctEnvironment) {
         score++;
         resultTxt.textContent = "Oikein!";
@@ -75,9 +78,11 @@ function checkAnswer(selected) {
         }, 3000);
     } else {
         resultTxt.textContent = "Väärin!";
-        // näytä oikeavastaus
+        correctTxt.textContent = `Oikea vastaus: ${currentAnimal.correctEnvironment}`;
+
         setTimeout(() => {
             resultBox.classList.add("d-none");
+            correctTxt.textContent = '';
   
             currentIndex++;
             if (currentIndex < animals.length) {
@@ -87,8 +92,7 @@ function checkAnswer(selected) {
             };
         }, 3000);
     }
-
-} 
+}
 
 function showFact() {
     factBox.classList.remove("d-none");
